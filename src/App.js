cd686ded1e019b6733087e29a7ca61553738d1ec
Janice Lee <meekukin@gmail.com>
2022-06-12 19:27:30 -0700

    Add new notes components

--- src/App.js
+++ src/App.js
@@ -22,9 +22,22 @@ const App = () => {
     },
   ]);
 
+  const addNote = (text) => {
+    // console.log(text);
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+
   return (
     <div className='container'>
-      <NotesList notes={notes} />
+      <NotesList notes={notes} handleAddNote={addNote} />
     </div>
   )
 }
