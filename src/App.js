7fe06146c0c690e09885f10200e2451ca06d965f
Janice Lee <meekukin@gmail.com>
2022-06-14 22:09:00 -0700

    Add deleteNote function

--- src/App.js
+++ src/App.js
@@ -34,10 +34,15 @@ const App = () => {
     setNotes(newNotes);
   }
 
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
+
 
   return (
     <div className='container'>
-      <NotesList notes={notes} handleAddNote={addNote} />
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote} />
     </div>
   )
 }
